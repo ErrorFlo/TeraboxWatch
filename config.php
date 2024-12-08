edf8136a919a89fc53b8f134f8f0a2b05b959f69
ErrorFlo <errorflotech@gmail.com>
2024-12-08 13:38:37 +0545

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'bot_token' => '7342147613:AAE5CP08HSKRQxCaT--aEwBm4Wl94Q4Q3Dg',
+    'channels' => ['https://t.me/+PdED80FNKeRjYmM9'],
+    'channels_on_check' => ['-1002165199001'],
+    'video_base_url' => 'https://tera.errorflo.tech/play.html?url=',
+    'admin_chat_id' => '7367777836',
+];
